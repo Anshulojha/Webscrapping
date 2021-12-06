e8140670ab61517c06c4bc1ba34227b34f4fc4fe
Anshul <56879768+Anshulojha@users.noreply.github.com>
2021-12-06 22:20:32 +0530

    Add files via upload

---
+++ acessHTMLfromWEBPAGE.py
@@ -0,0 +1,25 @@
+import requests
+from bs4 import BeautifulSoup
+import csv
+URL="http://www.values.com/inspirational-quotes"  
+r=requests.get(URL)
+# print(r.content) 
+soup=BeautifulSoup(r.content,'html5lib')#this create obj of soup,r.content:raw HTML content. html5lib : Specifying the HTML parser we want to use.
+# print(soup.prettify()) #soup.prettify() :give visual reporessnt of parse treecreated from raw html. 
+quotes=[]
+table=soup.find('div',attrs={'id':'all_quotes'})
+# print(table.prettify())
+for row in table.findAll('div',attrs={'class':'col-6 col-lg-3 text-center margin-30px-bottom sm-margin-30px-top'}):
+    quote={}
+    quote['theme']=row.h5.text
+    quote['url']=row.a['href']
+    quote['img']=row.img['src']
+    quote['lines']=row.img['alt'].split(" #")[0]
+    quote['author']=row.img['alt'].split(" #")[1]
+    quotes.append(quote)
+filename='inspirational_quotes.csv'
+with open(filename,'w',newline='') as f:
+    w=csv.DictWriter(f,['theme','url','img','lines','author'])
+    w.writeheader()
+    for quote in quotes:
+        w.writerow(quote)
